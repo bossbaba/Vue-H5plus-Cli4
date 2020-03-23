987e4cd7c31ef71f16a14a9644745c488d6f1778
mrdotyan <449107144@qq.com>
2020-03-23 16:20:58 +0800

    项目迁移cli2.x到cli4.x

---
+++ src/h5plus/index.js
@@ -0,0 +1,8 @@
+import Navigator from "@/h5plus/navigator"
+import Accelerometer from '@/h5plus/accelerometer'
+import Runtime from "@/h5plus/runtime"
+import Vue from 'vue'
+
+Vue.use(Navigator)
+Vue.use(Accelerometer)
+Vue.use(Runtime)
